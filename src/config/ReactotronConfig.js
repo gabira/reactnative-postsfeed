abdc768bd5e4f55373f0fb09aed492f9074d218d
Gabriel Silveira <garrsilveira@gmail.com>
2019-05-29 23:42:47 -0300

    Inicializando todo o processo react-native

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,7 @@
+import Reactotron from 'reactotron-react-native';
+
+if (__DEV__) {
+  Reactotron.configure({ host: '192.168.56.1' }) // controls connection & communication settings
+    .useReactNative() // add all built-in react native plugins
+    .connect(); // let's connect!
+}
